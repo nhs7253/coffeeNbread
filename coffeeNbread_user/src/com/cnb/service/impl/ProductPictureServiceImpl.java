5ef4625a96d472f4681af346cf2821147cfbeca1
Choi Minhee <minhee310@naver.com>
2017-06-30 21:44:27 +0900

    coffeeNbread_user
    ProductPicture 관련 모두 생성 및 수정
    
    FileUploadController 생성
    
    request2.jsp
    response2.jsp
    생성 - 파일 업로드 확인용
    
    ProductMapper.xml 수정
    
    ProductService 수정
    
    web-context.xml
    web.xml
    수정 - MultipartFile

---
+++ coffeeNbread_user/src/com/cnb/service/impl/ProductPictureServiceImpl.java
@@ -0,0 +1,43 @@
+package com.cnb.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cnb.dao.ProductPictureDao;
+import com.cnb.service.ProductPictureService;
+import com.cnb.vo.ProductPicture;
+
+/*
+ * 최민희
+ * 2017-06-30
+ * 생성
+ */
+@Service
+public class ProductPictureServiceImpl implements ProductPictureService {
+	
+	@Autowired
+	private ProductPictureDao dao;
+
+	@Override
+	public int addProductPicture(ProductPicture productPicture) {
+		return 0;
+	}
+
+	@Override
+	public int modifyProductPictureByProductPicture(ProductPicture productPictureVO, String productPicture) {
+		return 0;
+	}
+
+	@Override
+	public int removeProductPicture(ProductPicture productPicture) {
+		return 0;
+	}
+
+	@Override
+	public List<ProductPicture> findProductPictureListByProductIdAndStoreId(String productId, String storeId) {
+		return null;
+	}
+	
+}
