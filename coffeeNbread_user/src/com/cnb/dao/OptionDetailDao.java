55df9e6596db9ec6777f3a0c14d20e3162836840
Lee Jinyoung <wlsdud20923@naver.com>
2017-07-04 12:37:16 +0900

    coffeeNbread_user
    OptionCategory.java
    OptionCategoryDao.java
    OptionCategoryDaoImpl.java
    OptionCategoryDaoImplTest.java
    OptionCategoryMapper.xml
    OptionDetailDao.java
    OptionDetailDaoImpl.java
    OptionDetailDaoImplTest.java
    OptionDetailMapper.xml			
    							OptionCategory / OptionDetail
    							Service 생성 및 나머지 파일들 수정

--- coffeeNbread_user/src/com/cnb/dao/OptionDetailDao.java
+++ coffeeNbread_user/src/com/cnb/dao/OptionDetailDao.java
@@ -1,5 +1,7 @@
 package com.cnb.dao;
 
+import java.util.List;
+
 import com.cnb.vo.OptionCategory;
 import com.cnb.vo.OptionDetail;
 
@@ -33,6 +35,35 @@ public interface OptionDetailDao {
 	 * @param OptionDetail 
 	 * @return 처리 개수
 	 */
-	int deleteOptionDetail(int optionId);
+	int deleteOptionDetail(String storeId,int optionId);
 	
+	/**
+	 * 제품아이디로 옵션디테일정보 삭제 
+	 * @param storeId
+	 * @param productId
+	 * @return
+	 */
+	int deleteOptionDetailByProductId(String storeId,String productId);
+	
+	/**
+		 * 옵션카테고리로 옵션 디테일  정보리스트 select
+		 * @param storeId, OptionId
+		 * @return 조회된 옵션 디테일
+		 */
+	List<OptionDetail> selectOptionDetailListByOptionCategory(String storeId,int OptionId);
+
+	/**
+	 * 옵션디테일 이름으로 옵션 디테일  정보 select
+	 * @param storeId, OptionId
+	 * @return 조회된 옵션 디테일
+	 */
+	OptionDetail selectOptionDetailByOptionDetailName(String storeId,int optionId ,String optionCategoryDetail);
+
+	/**
+	 * 제품아이디로 옵션 디테일 정보 select
+	 * @param storeId, optionId, optionCategoryDetail
+	 * @return 조회된 옵션 디테일
+	 */
+	OptionDetail selectOptionDetailByProductId(String storeId,String productId);
+
 }
